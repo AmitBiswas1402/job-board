eba45eaa088f61adc8a863e8595da948b80185a8
AmitBiswas1402 <amit.142biswas@gmail.com>
2025-08-27 16:19:22 +0530

    feat: refactor Clerk integration; create custom ClerkProvider, add dark mode support, and update environment variables

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/ThemeProvider";
-import { ClerkProvider } from "@clerk/nextjs";
+import { ClerkProvider } from "./services/clerk/components/ClerkProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
